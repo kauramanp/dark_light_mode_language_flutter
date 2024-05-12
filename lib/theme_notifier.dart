9d827d19a7b01bbb167f2bacb1ab24ed8e42abac
kauramanp <33446281+kauramanp@users.noreply.github.com>
2024-05-13 03:39:17 +0530

    Changes

--- lib/theme_notifier.dart
+++ lib/theme_notifier.dart
@@ -6,9 +6,7 @@ class ThemeNotifier with ChangeNotifier {
   ThemeNotifier({ThemeMode mode = ThemeMode.light}) : _mode = mode;
 
   void toggleMode() {
-    print("in toggle mode $_mode");
     _mode = _mode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
-    print("in toggle mode after $_mode");
     notifyListeners();
   }
 }
